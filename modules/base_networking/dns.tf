02d00a7d42306d0edd2caf8bbf40c0fa761442b3
Tim Fletcher <tfletcher@thoughtworks.com>
2019-06-06 19:37:21 +0200

    tf Bugfix provider v2 failure

--- modules/base_networking/dns.tf
+++ modules/base_networking/dns.tf
@@ -1,5 +1,7 @@
 resource "aws_route53_zone" "private_zone" {
   name   = "${var.private_dns_zone_name}"
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc {
+    vpc_id = "${aws_vpc.vpc.id}"
+  }
   tags   = "${local.common_tags}"
 }
--- modules/bastion/ami.tf
+++ modules/bastion/ami.tf
@@ -1,12 +1,6 @@
 data "aws_ami" "amazon_linux_2" {
   most_recent = true
-
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
+  owners = ["amazon"]
 
   filter {
     name   = "name"
