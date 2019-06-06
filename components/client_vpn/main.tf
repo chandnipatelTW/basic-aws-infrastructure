1bc284770a712eae4c89f3ef43df92de9b8c0bed
Tim Fletcher <tfletcher@thoughtworks.com>
2019-06-06 13:32:29 +0200

    tf Add 2 static ARNs for certs as a test

--- components/client_vpn/main.tf
+++ components/client_vpn/main.tf
@@ -22,6 +22,6 @@ module "client_vpn" {
   subnet_ids                 = "${data.terraform_remote_state.base_networking.public_subnet_ids}"
   deployment_identifier      = "data-eng-${var.cohort}"
   client_cidr_block          = "10.10.0.0/16"
-  server_cert_arn            = ""
-  root_certificate_chain_arn = ""
+  server_cert_arn            = "arn:aws:acm:ap-southeast-1:618886044591:certificate/2df22a1e-500d-4933-9bed-1c4ecb5e0c15"
+  root_certificate_chain_arn = "arn:aws:acm:ap-southeast-1:618886044591:certificate/a21e1ff4-21b6-4997-9080-9649e7a0bd37"
 }
