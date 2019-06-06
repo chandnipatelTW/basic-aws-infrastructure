67c85d4796dc8da3d1be76ada81280863d0ac3cb
Tim Fletcher <tfletcher@thoughtworks.com>
2019-06-06 18:41:35 +0200

    tf Add security group config for internet access

--- modules/client_vpn/client_vpn.tf
+++ modules/client_vpn/client_vpn.tf
@@ -27,3 +27,25 @@ resource "aws_ec2_client_vpn_network_association" "client_vpn" {
   subnet_id              = "${element(var.subnet_ids, count.index)}"
   count                  = "${length(var.subnet_ids)}"
 }
+
+resource "aws_security_group_rule" "client_vpn_ingress" {
+  security_group_id = "${element(aws_ec2_client_vpn_network_association.client_vpn.security_groups, count.index)}"
+  from_port         = 0
+  protocol          = "-1"
+  to_port           = 0
+  type              = "ingress"
+  count             = "${length(aws_ec2_client_vpn_network_association.client_vpn.security_groups)}"
+  description       = "Unrestricted ingress for Client VPN"
+  cidr_blocks       = ["${var.client_cidr_block}"]
+}
+
+resource "aws_security_group_rule" "client_vpn_egress" {
+  security_group_id = "${element(aws_ec2_client_vpn_network_association.client_vpn.security_groups, count.index)}"
+  from_port         = 0
+  protocol          = "-1"
+  to_port           = 0
+  type              = "egress"
+  count             = "${length(aws_ec2_client_vpn_network_association.client_vpn.security_groups)}"
+  description       = "Unrestricted ingress for Client VPN"
+  cidr_blocks       = ["${var.client_cidr_block}"]
+}
