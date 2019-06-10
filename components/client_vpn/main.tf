4749cc6d2d2e79bb5f327f0fef64b583e202a943
Tim Fletcher <tfletcher@thoughtworks.com>
2019-06-10 13:15:17 +0200

    tf Look up the correct ARNs from AWS, this assumes they are named correctly

--- components/client_vpn/main.tf
+++ components/client_vpn/main.tf
@@ -16,12 +16,24 @@ data "terraform_remote_state" "base_networking" {
   }
 }
 
+data "aws_acm_certificate" "root" {
+  domain      = "root.${var.cohort}.training"
+  types       = ["IMPORTED"]
+  most_recent = true
+}
+
+data "aws_acm_certificate" "server" {
+  domain      = "openvpn.${var.cohort}.training"
+  types       = ["IMPORTED"]
+  most_recent = true
+}
+
 module "client_vpn" {
   source                     = "../../modules/client_vpn"
   subnet_ids                 = "${data.terraform_remote_state.base_networking.public_subnet_ids}"
   security_group_id          = "${data.terraform_remote_state.base_networking.vpc_default_security_group_id}"
   deployment_identifier      = "data-eng-${var.cohort}"
   client_cidr_block          = "10.10.0.0/16"
-  server_cert_arn            = "arn:aws:acm:ap-southeast-1:618886044591:certificate/b78d65b0-1fe0-49c0-9e29-2c7c74a750f5"
-  root_certificate_chain_arn = "arn:aws:acm:ap-southeast-1:618886044591:certificate/9627ceb2-8c63-439e-b26e-3e42d50db014"
+  server_cert_arn            = "${data.aws_acm_certificate.server.arn}"
+  root_certificate_chain_arn = "${data.aws_acm_certificate.root.arn}"
 }
