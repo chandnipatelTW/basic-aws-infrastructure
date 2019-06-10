1ec76301f61045a20757ad77dbff8f0ca198e5b0
Tim Fletcher <tfletcher@thoughtworks.com>
2019-06-10 13:54:31 +0200

    tf Refactor manage.sh script to ensure that files are present

--- CA/manage.sh
+++ CA/manage.sh
@@ -1,85 +1,93 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-
-aws_import ()
+ensure_basic_setup ()
 {
-aws acm import-certificate \
-    --certificate file://certs/${TRAINING_COHORT}-root.pem \
-    --private-key file://certs/${TRAINING_COHORT}-root-key.pem \
-    --region=${AWS_DEFAULT_REGION}
-aws acm import-certificate \
-    --certificate file://certs/openvpn.${TRAINING_COHORT}.training.pem \
-    --private-key file://certs/openvpn.${TRAINING_COHORT}.training-key.pem \
-    --certificate-chain file://certs/${TRAINING_COHORT}-root.pem \
-    --region=${AWS_DEFAULT_REGION}
+    if ! cfssl version > /dev/null ; then
+        echo "Please install cfssl"
+        exit 1
+    fi
+
+    if [[ ! -d certs ]] ; then
+        echo "Making certs directory"
+        mkdir -p certs
+    fi
 }
 
-client_cert ()
+ensure_ca_init ()
 {
-    CN=${1}.${TRAINING_COHORT}.training
-    echo "Making client certificate"
-    sed -e s/%COMMONNAME%/${CN}/g < config/csr.json | \
-        cfssl gencert \
-        -ca certs/${TRAINING_COHORT}-root.pem \
-        -ca-key certs/${TRAINING_COHORT}-root-key.pem \
-        -config config/profiles.json \
-        -profile client \
-        -hostname ${CN} - |\
-        cfssljson -bare certs/${CN}
-}
-
-ensure_cfssl ()
-{
-# Requires cfssl
-if [[ ! -d certs ]] ; then
-    echo "Making certs directory"
-    mkdir -p certs
-fi
+    ensure_basic_setup
+    if [[ -f certs/${TRAINING_COHORT}-root.pem ]] ; then
+        echo No CA root found, please run $0 init
+        exit 1
+    fi
 }
 
 init_ca ()
 {
-CN=${TRAINING_COHORT}.training
+    ensure_basic_setup
+    CN=${TRAINING_COHORT}.training
+    echo "Making CA certificate for ${CN}"
 
-# Make the CA
-echo "Making CA certificate for ${CN}"
-
-sed -e s/%COMMONNAME%/${CN}/g < config/ca.json | \
-    cfssl genkey --initca - | \
-    cfssljson -bare certs/${TRAINING_COHORT}-root
-
+    sed -e s/%COMMONNAME%/${CN}/g < config/ca.json | \
+        cfssl genkey --initca - | \
+        cfssljson -bare certs/${TRAINING_COHORT}-root
 }
 
 server_cert ()
 {
-CN=openvpn.${TRAINING_COHORT}.training
+    ensure_ca_init
+    CN=openvpn.${TRAINING_COHORT}.training
+    echo "Making server certificate"
+    sed -e s/%COMMONNAME%/${CN}/g < config/csr.json | \
+     cfssl gencert \
+        -ca certs/${TRAINING_COHORT}-root.pem \
+        -ca-key certs/${TRAINING_COHORT}-root-key.pem \
+        -config config/profiles.json \
+        -profile server \
+        -hostname ${CN} - | \
+     cfssljson -bare certs/${CN}
+}
 
-# Make the server cert
-echo "Making server certificate"
+aws_import ()
+{
+    ensure_ca_init
+    aws acm import-certificate \
+        --certificate file://certs/${TRAINING_COHORT}-root.pem \
+        --private-key file://certs/${TRAINING_COHORT}-root-key.pem \
+        --region=${AWS_DEFAULT_REGION}
+    aws acm import-certificate \
+        --certificate file://certs/openvpn.${TRAINING_COHORT}.training.pem \
+        --private-key file://certs/openvpn.${TRAINING_COHORT}.training-key.pem \
+        --certificate-chain file://certs/${TRAINING_COHORT}-root.pem \
+        --region=${AWS_DEFAULT_REGION}
+}
 
-sed -e s/%COMMONNAME%/${CN}/g < config/csr.json | \
- cfssl gencert \
-    -ca certs/${TRAINING_COHORT}-root.pem \
-    -ca-key certs/${TRAINING_COHORT}-root-key.pem \
-    -config config/profiles.json \
-    -profile server \
-    -hostname ${CN} - | \
- cfssljson -bare certs/${CN}
+client_cert ()
+{
+    ensure_ca_init
+    CN=${1}.${TRAINING_COHORT}.training
+    echo "Making client certificate"
+    sed -e s/%COMMONNAME%/${CN}/g < config/csr.json | \
+        cfssl gencert \
+            -ca certs/${TRAINING_COHORT}-root.pem \
+            -ca-key certs/${TRAINING_COHORT}-root-key.pem \
+            -config config/profiles.json \
+            -profile client \
+            -hostname ${CN} - |\
+        cfssljson -bare certs/${CN}
 }
 
 usage ()
 {
     echo "init - create a new CA"
     echo "server - create AWS client VPN server certs"
-    echo "client <client name> create a client connection cert"
+    echo "client <client name> - create a client connection cert"
     echo "upload - Upload the CA and server certs to AWS"
     exit 1
 }
 
 CMD=${1:-usage}
-
-ensure_cfssl
 
 case ${CMD} in
     init) init_ca ;;
