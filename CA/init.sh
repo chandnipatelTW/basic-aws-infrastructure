0c3a7b21ba2402a1fe2739eea2657f20ddd05fc6
Tim Fletcher <tfletcher@thoughtworks.com>
2019-06-10 13:01:16 +0200

    tf Use training cohort as the domain name in the CA

--- CA/init.sh
+++ CA/init.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+set -eu -o pipefail
+CN=${TRAINING_COHORT}.training
+
 # Initialize an OpenVPN CA
 if ! which cfssl &>/dev/null; then
     echo "You need cfssl installed before you can continue"
+    echo "macOS: brew install cfssl"
     exit 1
 fi
 
 # Requires cfssl
-echo "# Making certs directory"
-mkdir -p certs
+if [[ ! -d certs ]] ; then
+    echo "Making certs directory"
+    mkdir -p certs
+fi
 
 # Make the CA
-echo "# Making CA certificate"
-cfssl genkey --initca config/ca.json | cfssljson -bare certs/ca
+echo "Making CA certificate for ${CN}"
 
+sed -e s/%COMMONNAME%/${CN}/g < config/ca.json | \
+    cfssl genkey --initca - | \
+    cfssljson -bare certs/ca
