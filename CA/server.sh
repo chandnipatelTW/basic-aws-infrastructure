378edda1f68ace8429958852cb13e9cfa6126aa8
Tim Fletcher <tfletcher@thoughtworks.com>
2019-06-10 13:22:34 +0200

    tf Remove input parsing from the server script

--- CA/server.sh
+++ CA/server.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-if [[ -z $1 ]]; then
-    echo "Usage: $0 SERVERNAME"
-    echo "Generates an openvpn server certificate"
-    exit 1
-else
-    CN=${1}.${TRAINING_COHORT}.training
-fi
-
-
+CN=openvpn.${TRAINING_COHORT}.training
 
 # Make the server cert
 echo "Making server certificate"
