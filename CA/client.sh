572d12d648a701f7e713ee39ae9ddec71436031a
Tim Fletcher <tfletcher@thoughtworks.com>
2019-06-10 13:01:50 +0200

    tf Use training cohort as the domain name in the client

--- CA/client.sh
+++ CA/client.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# Make a new client
+set -eu -o pipefail
+
 if [[ -z $1 ]]; then
     echo "Usage: $0 CLIENTNAME"
     echo "Generates a new static client certificate"
     exit 1
+else
+    CN=${1}.${TRAINING_COHORT}.training
 fi
 
-echo "# Making client certificate"
-cat config/csr.json | sed -e s/%COMMONNAME%/$1/g | \
+echo "Making client certificate"
+sed -e s/%COMMONNAME%/${CN}/g < config/csr.json | \
     cfssl gencert -ca certs/ca.pem -ca-key certs/ca-key.pem \
     -config="config/profiles.json" \
     -profile="client" \
-    -hostname="$1" - |\
-    cfssljson -bare "certs/$1"
+    -hostname="${CN}" - |\
+    cfssljson -bare "certs/${CN}"
