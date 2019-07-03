22a7e4dc41b3d41470c3baff3e0503dd422f20a6
Perla Villarreal <pvillarr09@gmail.com>
2019-07-03 15:34:38 +0530

    [Perla/Rahul] 8: Scale Kafka: Adding cloud-init script mounting volume

--- modules/training_kafka/ec2.tf
+++ modules/training_kafka/ec2.tf
@@ -5,6 +5,54 @@ resource "aws_instance" "kafka" {
   subnet_id              = "${var.subnet_id}"
   key_name               = "${var.ec2_key_pair}"
   iam_instance_profile   = "${aws_iam_instance_profile.kafka.name}"
+  user_data = <<-EOF
+    #!/bin/bash
+
+    function replace_property_value()
+    {
+      property_file=$1
+      property_name=$2
+      property_value=$3
+
+      sed -i '' -r "s/^${property_name}=(.*)$/${property_name}=${property_value}/g" ${property_file}
+    }
+
+    set -e
+
+    device_name="/dev/sdf"
+    dir_path="/data"
+    owner="cp-kafka"
+    owner_group="confluent"
+    kafka_data_dir="${dir_path}/kafka"
+    zookeeper_data_dir="${dir_path}/zookeeper"
+
+    mkfs -t xfs ${device_name}
+
+    mkdir ${dir_path}
+
+    cp /etc/fstab /etc/fstab.orig
+
+    echo "UUID=$(blkid ${device_name} -o value | head -n 1)  ${dir_path}  xfs  defaults,nofail  0  2" | tee -a /etc/fstab
+
+    mount -a
+
+    if [[ $? -eq 0 ]]
+    then
+      rm /etc/fstab.orig
+    else
+      rm /etc/fstab
+      mv /etc/fstab.orig /etc/fstab
+    fi
+
+    chown ${owner}:${owner_group} ${dir_path}
+
+    mkdir -p "${kafka_data_dir}"
+    mkdir -p "${zookeeper_data_dir}"
+
+    replace_property_value /etc/kafka/server.properties "log.dirs" "${kafka_data_dir}"
+    replace_property_value /etc/kafka/zookeeper.properties "dataDir" "${zookeeper_data_dir}"
+
+    EOF
 
   tags = "${merge(
     local.common_tags,
