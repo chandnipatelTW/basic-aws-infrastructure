46ed4419a0652d77c6458490af7ddb6377eccc1f
Rahul Bakale <rahuldb@thoughtworks.com>
2019-07-03 16:18:48 +0530

    [Perla/Rahul] 8: Scale Kafka: Escape cloud-init script variables with $$

--- modules/training_kafka/ec2.tf
+++ modules/training_kafka/ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "kafka" {
   subnet_id              = "${var.subnet_id}"
   key_name               = "${var.ec2_key_pair}"
   iam_instance_profile   = "${aws_iam_instance_profile.kafka.name}"
-  user_data = <<-EOF
+  user_data = <<-EOT
     #!/bin/bash
 
     function replace_property_value()
@@ -14,7 +14,7 @@ resource "aws_instance" "kafka" {
       property_name=$2
       property_value=$3
 
-      sed -i '' -r "s/^${property_name}=(.*)$/${property_name}=${property_value}/g" ${property_file}
+      sed -i '' -r "s/^$${property_name}=(.*)$/$${property_name}=$${property_value}/g" $${property_file}
     }
 
     set -e
@@ -23,16 +23,16 @@ resource "aws_instance" "kafka" {
     dir_path="/data"
     owner="cp-kafka"
     owner_group="confluent"
-    kafka_data_dir="${dir_path}/kafka"
-    zookeeper_data_dir="${dir_path}/zookeeper"
+    kafka_data_dir="$${dir_path}/kafka"
+    zookeeper_data_dir="$${dir_path}/zookeeper"
 
-    mkfs -t xfs ${device_name}
+    mkfs -t xfs $${device_name}
 
-    mkdir ${dir_path}
+    mkdir $${dir_path}
 
     cp /etc/fstab /etc/fstab.orig
 
-    echo "UUID=$(blkid ${device_name} -o value | head -n 1)  ${dir_path}  xfs  defaults,nofail  0  2" | tee -a /etc/fstab
+    echo "UUID=$(blkid $${device_name} -o value | head -n 1)  $${dir_path}  xfs  defaults,nofail  0  2" | tee -a /etc/fstab
 
     mount -a
 
@@ -44,15 +44,15 @@ resource "aws_instance" "kafka" {
       mv /etc/fstab.orig /etc/fstab
     fi
 
-    chown ${owner}:${owner_group} ${dir_path}
+    chown $${owner}:$${owner_group} $${dir_path}
 
-    mkdir -p "${kafka_data_dir}"
-    mkdir -p "${zookeeper_data_dir}"
+    mkdir -p "$${kafka_data_dir}"
+    mkdir -p "$${zookeeper_data_dir}"
 
-    replace_property_value /etc/kafka/server.properties "log.dirs" "${kafka_data_dir}"
-    replace_property_value /etc/kafka/zookeeper.properties "dataDir" "${zookeeper_data_dir}"
+    replace_property_value /etc/kafka/server.properties "log.dirs" "$${kafka_data_dir}"
+    replace_property_value /etc/kafka/zookeeper.properties "dataDir" "$${zookeeper_data_dir}"
 
-    EOF
+    EOT
 
   tags = "${merge(
     local.common_tags,
